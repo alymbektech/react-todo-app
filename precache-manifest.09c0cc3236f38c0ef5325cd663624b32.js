eef2fc7ea9075132b9aea49daa49781c97c25685
Alymbek Damir Uulu <damiruulu@marshall.edu>
2020-08-25 17:29:26 -0400

    Updates

---
+++ precache-manifest.09c0cc3236f38c0ef5325cd663624b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50692f23cb592308c1f56d48d3dc905",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "c19d0126242df8de99f6",
+    "url": "/react-todo-app/static/css/main.a4507ccd.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/react-todo-app/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19d0126242df8de99f6",
+    "url": "/react-todo-app/static/js/main.98cad2de.chunk.js"
+  },
+  {
+    "revision": "a02306a5582400e4c981",
+    "url": "/react-todo-app/static/js/runtime-main.15b7a15e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
